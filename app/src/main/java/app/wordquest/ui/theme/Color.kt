e5040e598149ebcf772de28f73b1acac731da4bb
NotRinKk <rinm4te@gmail.com>
2024-11-25 00:28:45 +0300

    Add: colors

--- app/src/main/java/app/wordquest/ui/theme/Color.kt
+++ app/src/main/java/app/wordquest/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package app.wordquest.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkPurple = Color(0xFF6E3099)
+val Lavender = Color(0xFFAC7ACF)
+val CarrotOrange = Color(0xFFFF8800)
+val YellowOrange = Color(0xFFF7B801)
+val CloudyGrey = Color(0xFF6B6565)
+val LightGrey = Color(0xFFD9D9D9)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
